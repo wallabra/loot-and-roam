d661a9d43a745b7181a613b036264b2acdef3e3a
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2025-02-19 14:35:59 -0300

    Add bevy main function stub
    Despite the comments atop main.rs, it is unclear whether we should
    pursue an actual multiplayer implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 //! # Application entrypoint for Loot & Roam.
 //!
-//! Currently a fairly thin wrapper around the app subtree.
+//! Every running program has in it a server and a client.
+//!
+//! On non-authoritative instances, the server exists for network balancing.
+//! The client can connect to another instance to interact with the
+//! authoritative instance; in authoritative instances, the client is merely
+//! for player interaction and server control.
+//!
+//! The program can also be run "headless", that is, without displaying the
+//! game.
 
 // Written by:
 // * Gustavo Ramos Rehermann <rehermann6046@gmail.com>
@@ -15,6 +23,20 @@
 // Loot & Roam comes with ABSOLUTELY NO WARRANTY, to the extent
 // permitted by applicable law.  See the CNPL for details.
 
+use bevy::{ecs::world::DeferredWorld, prelude::*};
+
+/// A Tokio runtime wrapped in a Bevy resource.
+#[derive(Resource)]
+struct TokioRuntime(pub(crate) tokio::runtime::Runtime);
+
 fn main() {
-    todo!();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(TokioRuntime(
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap(),
+        ))
+        .run();
 }
