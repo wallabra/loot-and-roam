1a11869d29d2d9730de78c027f7b5c408ed8d3a0
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2025-03-13 22:30:17 -0300

    Show phys points in cube demo

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -29,7 +29,6 @@ use crate::common::physics::{Gravity, NormalSpring, PhysPoint, PointNetwork};
 // pub mod input;
 // pub mod ui;
 
-/// Temporary tick update to check performance
 pub fn rotate(mut cubes: Query<(&mut Transform, &Rotatable)>, timer: Res<Time>) {
     for (mut transform, cube) in &mut cubes {
         // The speed is first multiplied by TAU which is a full rotation (360deg) in radians,
@@ -45,20 +44,28 @@ pub struct Rotatable {
     speed: f32,
 }
 
+/// Point netowrk snapping market component.
+#[derive(Component)]
+pub struct SnapToPointNet;
+
 pub fn apply_app_systems(app: &mut App) {
     app.add_systems(
         Update,
-        |mut query: Query<(&mut Transform, &PointNetwork)>| {
+        |mut query: Query<(&mut Transform, &PointNetwork), With<SnapToPointNet>>| {
             for (mut transform, network) in query.iter_mut() {
                 if !network.points.is_empty() {
                     // Center Transform on average of all physics points
-                    let avg: ultraviolet::Vec3 = network
-                        .points
-                        .iter()
-                        .map(|point| point.pos)
-                        .reduce(|acc, vec| acc + vec)
-                        .unwrap()
-                        / network.points.len() as f32;
+                    // let len = network.points.len() as f32;
+                    // let avg: ultraviolet::Vec3 = network
+                    //     .points
+                    //     .iter()
+                    //     .map(|point| point.pos)
+                    //     .fold(ultraviolet::Vec3::zero(), |acc, pos| acc + pos);
+                    // let avg = avg * 2.0 / len;
+
+                    // [TEMP] Center on first point for debugging
+                    let avg = network.points[0].pos.clone();
+
                     transform.translation.x = avg.x;
                     transform.translation.y = avg.y;
                     transform.translation.z = avg.z;
@@ -68,6 +75,30 @@ pub fn apply_app_systems(app: &mut App) {
             }
         },
     );
+
+    app.add_systems(Update, (debug_point_attach_snap,));
+}
+
+// -- Show physics points for debugging purposes
+
+/// Component attached to physics points when debugging
+#[derive(Component)]
+struct DebugPointAttach {
+    point_idx: usize,
+}
+
+fn debug_point_attach_snap(
+    mut query_child: Query<(&Parent, &mut Transform, &DebugPointAttach)>,
+    query_parent: Query<&PointNetwork>,
+) {
+    for (parent, mut transform, attachment) in query_child.iter_mut() {
+        let parent_points: &PointNetwork = query_parent.get(parent.get()).unwrap();
+
+        let point = &parent_points.points[attachment.point_idx].pos;
+        transform.translation.x = point.x;
+        transform.translation.y = point.y;
+        transform.translation.z = point.z;
+    }
 }
 
 /// Bevy setup system for the main Loot & Roam application.
@@ -80,7 +111,11 @@ pub fn setup(
     // circular base
     commands.spawn((
         Mesh3d(meshes.add(Circle::new(4.0))),
-        MeshMaterial3d(materials.add(Color::WHITE)),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::Srgba(Color::WHITE.to_srgba().with_alpha(0.6)),
+            alpha_mode: AlphaMode::Multiply,
+            ..Default::default()
+        })),
         Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2))
             .with_translation(Vec3::new(0.0, -0.5, 0.0)),
     ));
@@ -88,21 +123,22 @@ pub fn setup(
     // cube
     let mut points = PointNetwork::from(
         [
+            [-0.5, -0.5, -0.5],
+            [-0.5, -0.5, 0.5],
             [-0.5, 0.5, -0.5],
             [-0.5, 0.5, 0.5],
-            [-0.5, 1.5, -0.5],
-            [-0.5, 1.5, 0.5],
+            [0.5, -0.5, -0.5],
+            [0.5, -0.5, 0.5],
             [0.5, 0.5, -0.5],
             [0.5, 0.5, 0.5],
-            [0.5, 1.5, -0.5],
-            [0.5, 1.5, 0.5],
         ]
         .map(|arr| PhysPoint::from_pos(ultraviolet::Vec3::from(arr)))
         .into_iter(),
     );
     let springs = points.make_radially_connected_springs(
         crate::common::physics::SpringMode::Normal(NormalSpring { stiffness: 10.0 }),
-        1.5, // bigger than sqrt(2) so should connect faces diagonally too
+        1.5, // bigger than sqrt(2) so should connect face vertices diagonally too
+             // but smaller than cbrt(3) so should not connect fully opposite vertices
     );
 
     info!(
@@ -112,21 +148,43 @@ pub fn setup(
     );
 
     // [NOTE] temporary test tug, or TTT for short :D
-    points.points[3].vel.y += 12.0;
-    points.points[3].vel.x += 5.0;
-    //
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-        Rotatable { speed: 0.3 },
-        points,
-        springs,
-        Gravity {
-            // low grav for development purposes
-            force: ultraviolet::Vec3::unit_y() * -0.5,
-        },
-    ));
+    points.points[3].vel.y += 3.0;
+    points.points[3].vel.x += 1.0;
+
+    let num_points = points.points.len();
+
+    let cube = commands
+        .spawn((
+            Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: Color::srgba_u8(124, 144, 255, 140),
+                alpha_mode: AlphaMode::Blend,
+                ..Default::default()
+            })),
+            Transform::default(),
+            Rotatable { speed: 0.1 },
+            points,
+            springs,
+            Gravity {
+                // low grav for development purposes
+                force: ultraviolet::Vec3::unit_y() * -0.2,
+            },
+            SnapToPointNet,
+        ))
+        .id();
+
+    for point_idx in 0..num_points {
+        let child_point = commands
+            .spawn((
+                DebugPointAttach { point_idx },
+                Mesh3d(meshes.add(Sphere::new(0.04))),
+                MeshMaterial3d(materials.add(Color::srgb_u8(255, 255, 64))),
+                Transform::default(),
+            ))
+            .id();
+
+        commands.entity(cube).add_child(child_point);
+    }
 
     // light
     commands.spawn((
